391e9fe8837a0d3f5cffc15220502993b951466b
Rey Lejano <rlejano@gmail.com>
2024-08-01 13:34:52 -0700

    add jq install to kind install script

--- install-kind-cilium.sh
+++ install-kind-cilium.sh
@@ -12,6 +12,10 @@ sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 sudo usermod -aG docker $USER
 sudo systemctl start docker
 
+# install jq
+echo "Installing jq"
+sudo yum install -y jq
+
 # install kind for AMD64/x86_64
 echo "Installing kind"
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-amd64
